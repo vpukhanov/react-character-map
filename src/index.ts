776ae469c470f94a67159a244cca7c075273d308
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2020-05-29 11:02:48 +0300

    feat: export CharMapContext for advanced use cases

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import CharMap from "./components/CharMap/CharMap";
 import CharMapCategoriesList from "./components/CharMapCategoriesList/CharMapCategoriesList";
 import CharMapContextProvider from "./components/CharMapContextProvider/CharMapContextProvider";
 import CharMapSelector from "./components/CharMapSelector/CharMapSelector";
+import CharMapContext from "./context/CharMapContext";
 
 export { CharMap };
 export { CharMapSelector };
 export { CharMapCategoriesList };
 export { CharMapContextProvider };
+export { CharMapContext };
 
 export default CharMap;
