d6f7963a647fd879bd745eeff2c2948b88586ffa
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2020-05-29 07:15:50 +0300

    chore: reorganize the file structure

--- src/index.js
+++
@@ -1,2 +0,0 @@
-import CharacterMap from "./component/CharacterMap";
-export { CharacterMap };
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './src/index.js',
+    entry: './src/index.ts',
     output: {
       filename: 'index.js',
       path: path.resolve(__dirname, 'dist'),
