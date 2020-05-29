f06ebe393f021d40f1344d4c054e8609e806913f
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2020-05-29 07:12:23 +0300

    build: switch to TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,12 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.tsx?$/,
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
+                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
                     }
                 }
             },
